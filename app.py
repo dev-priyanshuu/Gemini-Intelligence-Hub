a8e1f31041f86f8c9e05c386046bd5d2efbaf20e
priyanshu shukla <78398690+priyxnshuuu@users.noreply.github.com>
2024-08-07 00:45:47 +0530

    add Mental Therapist to main app

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from streamlit_option_menu import option_menu
 from YoutubeSummarizer.youtube_summarizer import get_transcript_text, generate_gemini_content
 from ChatWithPDF.chat_with_pdf import user_input, get_pdf_text, get_text_chunks, get_vector_store
 from Vision.vision import get_gemini_response
+from MentalTherapist.mental_therapist import start_chat, get_bot_response
+
 from PIL import Image
 
 # Set page title and configuration
@@ -14,9 +16,10 @@ with st.sidebar:
     project_selection = option_menu('Google Gemini Projects',
                           ['Youtube Summarizer',
                            'Chat with PDF',
-                           'Google Vision'],
-                          icons=['youtube','book','eye'],
-                          default_index=0)
+                           'Google Vision',
+                           'Mental Therapist'],
+                          icons=['youtube', 'file-text', 'eye', 'heart'],
+                          default_index=0)  
 
 # Main content area
 if project_selection == 'Youtube Summarizer':
@@ -79,4 +82,78 @@ elif project_selection == 'Google Vision':
             st.subheader("The Response is")
             st.write(response)
 
+elif project_selection == 'Mental Therapist':
+    if 'chat' not in st.session_state:
+        st.session_state.chat = start_chat()
 
+    # Streamlit application
+    st.title("Mental Health Chatbot")
+
+    # Display conversation history
+    if "conversation_history" not in st.session_state:
+        st.session_state.conversation_history = []
+
+    # Inject custom CSS for button styling
+    st.markdown("""
+        <style>
+        .stButton > button {
+            background-color: #0e1117; /* Initial button color */
+            color: white; /* Text color */
+            margin-top:25px;
+            border: none; /* Remove border */
+            border-radius: 5px; /* Rounded corners */
+            padding: 5px 15px; /* Padding */
+            cursor: pointer; /* Pointer cursor on hover */
+            font-size: 16px; /* Font size */
+        }
+        .stButton > button:hover {
+            background-color: gray; /* Color on hover */
+        }
+        </style>
+    """, unsafe_allow_html=True)
+
+    # Create a form for the input box and button
+    with st.form(key='chat_form', clear_on_submit=True):
+        # Create a horizontal layout for the input box and button
+        col1, col2 = st.columns([8 , 1])  # Adjust column widths as needed
+
+        with col1:
+            # Input box for user message
+            user_input = st.text_input("You:", "")
+
+        with col2:
+            # Add a button to send the message
+            submit_button = st.form_submit_button("Send")
+
+        if submit_button and user_input:
+            # Append user message to conversation history
+            st.session_state.conversation_history.append(f"User: {user_input}")
+
+            # Show loading spinner while waiting for response
+            with st.spinner('Waiting for the bot to respond...'):
+                # Get response from AI handler
+                bot_response = get_bot_response(st.session_state.chat, user_input)
+
+            # Append bot response to conversation history
+            st.session_state.conversation_history.append(f"Bot: {bot_response}")
+
+    # Display updated conversation history
+    for message in st.session_state.conversation_history:
+        if message.startswith("User:"):
+            st.markdown(
+                f'''
+                <div style="border: 2px solid gray; border-radius: 5px; padding: 10px; margin-bottom: 10px; ">
+                    <strong>{message}</strong>
+                </div>
+                ''',
+                unsafe_allow_html=True
+            )
+        else:
+            st.markdown(
+                f'''
+                <div style="padding: 10px; margin-bottom: 10px; ">
+                    <strong>{message}</strong>
+                </div>
+                ''',
+                unsafe_allow_html=True
+            )
